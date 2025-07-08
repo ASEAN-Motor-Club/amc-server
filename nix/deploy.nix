4ece1f435e559b1177e94fa2b7f70c10aba26687
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-07-08 22:48:05 +0700

    deploy with submodules

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -82,7 +82,7 @@ pkgs.writeShellScriptBin "deploy" ''
   echo "--------------------------------"
 
 
-  NIX_SSHOPTS="-p $SSH_PORT" ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$TARGET_HOST" --build-host "$TARGET_HOST" --flake . --fast switch
+  NIX_SSHOPTS="-p $SSH_PORT" ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$TARGET_HOST" --build-host "$TARGET_HOST" --flake .?submodules=1# switch
 
   exit 0
 ''
