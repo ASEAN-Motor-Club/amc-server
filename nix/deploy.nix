7cb774c09f804ad92b1abf904fc853d4f9c441ea
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-12-22 22:10:39 +0700

    override amc-backend with local path on deploy

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -2,6 +2,6 @@
 pkgs.writeShellScriptBin "deploy" ''
   set -xe
 
-   ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$1" --build-host "$1" --flake . --fast switch
+   ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$1" --build-host "$1" --flake . --fast switch --override-input amc-backend ./amc-backend
 ''
 
