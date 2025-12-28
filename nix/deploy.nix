03bed53b5a1e91158011cfb75840c99950fd9385
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-12-28 10:14:05 +0700

    override inputs in deployment script

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -2,6 +2,8 @@
 pkgs.writeShellScriptBin "deploy" ''
   set -xe
 
-   ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$1" --build-host "$1" --flake . --fast switch --override-input amc-backend ./amc-backend
+  ${pkgs.nixos-rebuild}/bin/nixos-rebuild --target-host "$1" --build-host "$1" --flake . --fast switch \
+  --override-input amc-backend ./amc-backend \
+  --override-input motortown-server ./motortown-server-flake
 ''
 
