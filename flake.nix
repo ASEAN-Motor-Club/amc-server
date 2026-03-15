943b26d657064fc3d95d1a9b94599a22a0056ba9
Freeman <4031216+fmnxl@users.noreply.github.com>
2026-03-15 21:11:18 +0700

    Bump test container to MotorTownMods v0.31.0-rc4

--- flake.nix
+++ flake.nix
@@ -128,7 +128,7 @@
                 enableMods = true;
                 restartSchedule = "3000-01-01 00:00:00";
                 betaBranch = "test";
-                modVersion = "v0.31.0-rc1";
+                modVersion = "v0.31.0-rc4";
                 enableExternalMods = {
                   qxZap_CranyUnlocked_P = true;
                   MajasDetailWorks7_17_P = true;
